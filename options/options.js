9cabfb80f03774c80bee59916a189f0b9d279a11
Crowdbotics <team@crowdbotics.com>
2021-09-19 15:15:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "supermarioytg_app_30680Identifier",
+  url: "https://supermarioytg_app_30680Identifier.botics.co",
+  api: "https://supermarioytg_app_30680Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
